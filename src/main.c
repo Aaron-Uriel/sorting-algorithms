9b2348ace6d8d44193e03647b1cbf0f542ac48ad
Aaron-Uriel <therialaaronuriel@gmail.com>
2023-05-06 20:50:47 -0600

    Agregar menú.

--- src/main.c
+++ src/main.c
@@ -1,21 +1,163 @@
 #include <stdio.h>
 #include <time.h>
-
-#include <assert.h>
+#include <locale.h>
+#include <stdlib.h>
 
 #include "sort.h"
+#include "utilities.h"
 
-void (*sorting_functions[])(int [], int, int) = { 
+/* Constantes. */
+#define OPTS_LIMIT 4      /* Cantidad de opciones en el menú */
+                          /* principal.                      */
+#define SORT_FUNC_LIMIT 1 /* Cantidad de algoritmos de       */
+                          /* ordenamiento.                   */
+
+/* Prototipos de funciones. */
+void fill_array(void);
+void print_array(void);
+void sorting_menu(void);
+void exit_program(void);
+
+/* Globales. */
+int *array; /* El arreglo que usa el programa es global. */
+unsigned int array_size;
+
+void (*menu_options[OPTS_LIMIT])(void) = { fill_array, print_array,
+    sorting_menu, exit_program};
+
+void (*sorting_functions[SORT_FUNC_LIMIT])(int [], int, int) = { 
     quicksort,
 };
+
+/* El menú principal del programa. */
+void main_menu() {
+    unsigned int selected_opt, inputs;
+    do {
+        printf("Algoritmos de ordenamiento.\n"
+                "Opciones del arreglo de prueba:\n");
+        printf("1.- Llenar.\n"
+               "2.- Mostrar.\n"
+               "3.- Ordenar.\n"
+               "4.- Salir.\n");
+
+        printf("Opción elegida: ");
+        inputs = scanf("%d", &selected_opt);
+        if (inputs != 1 || selected_opt > OPTS_LIMIT) {
+            printf("Opción incorrecta...\n");
+            clear_stdin();
+            pause();
+            continue;
+        }
+
+        menu_options[selected_opt - 1]();
+        pause();
+    } while (selected_opt != OPTS_LIMIT);
+}
+
+/*
+ * Se encarga de llenar el arreglo usado en el programa con números aleatorios.
+ * El arreglo es creado en la memoria dinámica, si ya se creó un arreglo con
+ * anterioridad, este se libera y se vuelve a crear uno nuevo.
+ */
+void
+fill_array()
+{
+    /* Si ya existe un arreglo creado, lo borramos. */
+    if (array != NULL) {
+        free(array);
+    }
+
+    unsigned int inputs, size;
+    do {
+        printf("¿De qué tamaño crear el arreglo?.\n"
+               "Tamaño: ");
+        inputs = scanf("%u", &size);
+        if (inputs != 1) {
+            printf("Tamaño no válido...");
+            clear_stdin();
+            pause();
+            continue;
+        }
+    } while(inputs != 1);
+
+    clock_t initial_time, final_time;
+    double delta_time;
+    initial_time = clock();
+    array = malloc(size * sizeof(*array));
+    array_size = size;
+    for (int i = 0; i < size; i += 1) {
+        array[i] = rand() % 512;
+    }
+    final_time = clock();
+    delta_time = (double)(final_time - initial_time) / CLOCKS_PER_SEC;
+    printf("Creado un arreglo de %d elementos en %g milisegundos\n",
+           size, delta_time * 1000);
+}
+
+/* Imprime el arreglo usado en el programa. */
+void
+print_array(void)
+{
+    printf("Arreglo.\n");
+    printf("Elementos: ");
+    for (int i = 0; i < array_size; i += 1) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    printf("El arreglo está: %s\n", 
+           (is_sorted(array, array_size))?"ordenado": "no ordenado");
+}
+
+/* Maneja el menú para escoger el algoritmo de ordenamiento. */
+void 
+sorting_menu(void)
+{
+    unsigned int selected_opt, inputs;
+    do {
+        printf("Ordenar arreglo.\n"
+                "¿Qué algoritmo de ordenamiento desea ejecutar?:\n");
+        printf("1.- Quicksort.\n");
+
+        printf("Opción elegida: ");
+        inputs = scanf("%d", &selected_opt);
+        if (inputs != 1 || selected_opt > SORT_FUNC_LIMIT) {
+            printf("Opción incorrecta...\n");
+            clear_stdin();
+            pause();
+            continue;
+        }
+    } while (selected_opt > SORT_FUNC_LIMIT);
+
+    clock_t initial_time, final_time;
+    double delta_time;
+    initial_time = clock();
+    sorting_functions[selected_opt - 1](array, 0, array_size - 1);
+    final_time = clock();
+    delta_time = (double)(final_time - initial_time) / CLOCKS_PER_SEC;
+
+    printf("Arreglo de %d elementos ordenado en %g milisegundos\n",
+            array_size,
+            delta_time * 1000);
+}
+
+/* Termina el programa de forma normal. */
+void
+exit_program(void)
+{
+    exit(0);
+}
 
 int
 main(void)
 {
-    /* Prueba rápida de que todo funciona bien. */
-    int32_t xd[4] = {10, 15, 1, 35};    
-    sorting_functions[0](xd, 0, 3);
-    assert(xd[0] == 1 && xd[2] == 15);
+    /* 
+     * Comprobar que las palabras con acentos se vean bien en Windows con esta
+     * función.
+     */
+    setlocale(LC_ALL, "");
+    srand(time(NULL));
+
+    main_menu();
 
     return 0;
 }
--- src/utilities.c
+++ src/utilities.c
@@ -1,11 +1,48 @@
 #include "utilities.h"
+#include <stdio.h>
 
 /* Intercambia dos valores. */
 void
-swap(int32_t *const a, int32_t *const b)
+swap(int *const a, int *const b)
 {
     int temp = *a;
 
     *a = *b;
     *b = temp;
+}
+
+/* Limpia todas los elementos por leer en stdin. */
+void
+clear_stdin(void)
+{
+    while (getchar() != '\n');
+}
+
+/* Imprime una 'pantalla de pausa'. */
+void
+pause(void)
+{
+    printf("Presione enter para continuar...");
+    while (getchar() != '\n');
+    while (getchar() != '\n');
+}
+
+/* Comprueba si un arreglo está ordenado. */
+bool
+is_sorted(int array[], int size) {
+    if (array == NULL || size == 0) {
+        return false;
+    }
+    if (size == 1) {
+        return true;
+    }
+
+    int i;
+    for (i = 0; (i + 1) < size; i += 1) {
+        if (array[i] > array[i + 1]) {
+            return false;
+        }
+    }
+
+    return true;
 }

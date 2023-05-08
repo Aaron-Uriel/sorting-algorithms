b1d05a4722c2f1a9e2c91b746c984e1d9cbbf2db
ferfabianreyes <2100721a@umich.mx>
2023-05-07 21:34:42 -0600

    función se inserción terminada

--- src/insertion.c
+++ src/insertion.c
@@ -3,5 +3,16 @@
 
 void insertion(int  array[], int first, int last)
 {
-    
+    int i, j, aux, n = last;
+
+    for (i = 1; i < n; i++)
+    {
+        j = 1;
+        aux = array[i];
+        while (j > 0 && aux < array[i]){
+            a[j] = a[j-1];
+            j--;
+        }
+        a[j] = aux;
+    }
 }

a324fc3d453163a4bc71759a70c786cfa167a4d9
Aaron-Uriel <therialaaronuriel@gmail.com>
2023-05-11 13:16:34 -0600

    Ahora el ExchangeSort si toma en cuenta el último número.

--- src/exchange.c
+++ src/exchange.c
@@ -3,9 +3,9 @@
 
 void exchange(int  array[], int first, int last)
 {
-    int i = first, j, n = last;
+    int i, j, n = last;
 
-    for (i; i <= n-2; i++)
+    for (i = first; i <= n-1; i++)
     {
         for (j = i+1; j <= n-1; j++)
         {

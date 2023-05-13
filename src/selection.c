393e0ab91ed7d61c7bf9fc88cd7e39b3fa1c5c6a
ferfabianreyes <2100721a@umich.mx>
2023-05-13 13:41:27 -0600

    selección ordena

--- src/selection.c
+++ src/selection.c
@@ -3,21 +3,21 @@
 
 void selection(int  array[], int first, int last)
 {
-    int menor, i = first, j, n = last;
+    int menor, i= first, j, n = last + 1;
 
     for (i; i < n-1; i++)
     {
         menor = i;
-        for (j = i+1; i < n; j++)
-        {
+        for (j = i+1; j < n; j++)
+        
             if (array[j] < array[menor])
-            {
+            
                 menor = j;
-            }
+            
             if (i != menor)
             {
                 swap(&array[i], &array[menor]);
             }
-        }
+        
     }
 }
